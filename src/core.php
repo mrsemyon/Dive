70b60ae19de203a427181236d84127599f62cf35
mrSemyon <sky_net@mail.ru>
2022-06-09 03:55:41 +0500

    Added required files in core.php

---
+++ src/core.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+
+require $_SERVER['DOCUMENT_ROOT'] . '/src/functions.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/src/classes.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/src/config.php';
