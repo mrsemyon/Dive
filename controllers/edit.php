ce76df7afac50a3d3bb3a7cec45f1476fe45424a
mrSemyon <syoma4k@gmail.com>
2022-06-10 01:01:02 +0500

    Added controller for edit user

---
+++ controllers/edit.php
@@ -0,0 +1,26 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$pdo = createPDO();
+$user = getUserById($pdo, $_POST['id']);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'У Вас недостаточно прав');
+    redirect('/users/');
+    exit;
+}
+
+$data = $_POST;
+
+foreach ($data as $key => $value) {
+    if ($value == '') {
+        $data[$key] = $user[$key];
+    }
+}
+
+setUserInfo($pdo, $_POST['id'], $data);
+
+setFlashMessage('success', 'The information has been successfully updated');
+redirect("/public/users.php");
+exit;
