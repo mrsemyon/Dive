14a70324dae388200d3e1465ce037b445928cd7b
mrSemyon <syoma4k@gmail.com>
2022-06-09 07:14:52 +0500

    Bugfix in function getUserByEmail()

--- src/functions/getUserByEmail.php
+++ src/functions/getUserByEmail.php
@@ -5,5 +5,5 @@ function getUserByEmail($pdo, $email)
     $sql = "SELECT * FROM users WHERE email=:email";
     $statement = $pdo->prepare($sql);
     $statement->execute(['email' => $email]);
-    return $statement->fetchAll();
+    return $statement->fetch();
 }
