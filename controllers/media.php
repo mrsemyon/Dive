ffe918f9338dad49efb84b15e05f96a2a771731e
mrSemyon <syoma4k@gmail.com>
2022-06-10 03:46:30 +0500

    Added controller for load photo

---
+++ controllers/media.php
@@ -0,0 +1,25 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$pdo = createPDO();
+$user = getUserById($pdo, $_POST['id']);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'You don\'t have enought rights');
+    redirect('/public/users.php');
+    exit;
+}
+if ($user['photo'] != 'no_photo.jpg') {
+    unlink($_SERVER['DOCUMENT_ROOT'] . '/upload/' . $user['photo']);
+}
+
+$photo = (! empty($_FILES['photo']['name']))
+	? prepareUserPhoto($_FILES['photo'])
+	: 'no_photo.jpg';
+
+setUserPhoto($pdo, $user['id'], $photo);
+
+setFlashMessage('success', 'The photo has been successfully updated');
+redirect("/public/users.php");
+exit;
