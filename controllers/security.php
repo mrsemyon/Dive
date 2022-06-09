dc2cbd11133ef92544e1f0fdba85eb72ec049ca3
mrSemyon <syoma4k@gmail.com>
2022-06-10 02:34:56 +0500

    Added controller for edit credentials

---
+++ controllers/security.php
@@ -0,0 +1,39 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$pdo = createPDO();
+$user = getUserById($pdo, $_GET['id']);
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'You don\'t have enought rights');
+    redirect('/public/users.php');
+    exit;
+}
+
+if (empty($_POST['password']) && ($user['email'] == $_POST['email'])) {
+    setFlashMessage('danger', 'Information hasn\'t been updated');
+    redirect('/public/users.php');
+    exit;
+}
+
+if (! empty($_POST['email'])) {
+    if ($_POST['email'] != $user['email']) {
+        if (!empty(getUserByEmail($pdo, $_POST['email']))) {
+            setFlashMessage('danger', 'This email address is already taken by another user.');
+            redirect("/public/security.php?id=" . $user['id']);
+            exit;
+        }
+        changeEmail($pdo, $user['id'], $_POST['email']);
+        if (($_SESSION['role'] != 'admin') || ($_SESSION['role'] == 'admin' && $user['role'] == 'admin')) {
+            $_SESSION['email'] = $_POST['email'];
+        }
+    }
+}
+
+if (! empty($_POST['password'])) {
+    changePassword($pdo, $user['id'], $_POST['password']);
+}
+
+setFlashMessage('success', 'The information has been successfully updated');
+redirect("/public/users.php");
+exit;
