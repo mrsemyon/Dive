ef7ac6f96686f92b5e2a9f6570ad21295677b47c
mrSemyon <syoma4k@gmail.com>
2022-06-10 02:55:53 +0500

    Added controller for set status

---
+++ controllers/status.php
@@ -0,0 +1,18 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$pdo = createPDO();
+$user = getUserById($pdo, $_POST['id']);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'You don\'t have enought rights');
+    redirect('/public/users.php');
+    exit;
+}
+
+setUserStatus($pdo, $user['id'], $_POST['status']);
+
+setFlashMessage('success', 'The information has been successfully updated');
+redirect("/public/users.php");
+exit;
