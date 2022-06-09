9c5c39ab08f4b4a80d67ab8623055e2e597c8e04
mrSemyon <syoma4k@gmail.com>
2022-06-10 04:00:38 +0500

    Added controller for delete user

---
+++ controllers/delete.php
@@ -0,0 +1,27 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$pdo = createPDO();
+$user = getUserById($pdo, $_GET['id']);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'You don\'t have enought rights');
+    redirect('/public/users.php');
+    exit;
+}
+
+deleteUser($pdo, $_GET['id']);
+
+if ($user['photo'] != 'no_photo.jpg') {
+    unlink($_SERVER['DOCUMENT_ROOT'] . '/upload/' . $user['photo']);
+}
+
+if ($user['email'] == $_SESSION['email']) {
+    session_destroy();
+    redirect('/public/authorization.php');
+    exit;
+}
+setFlashMessage('success', 'User deleted successfully');
+redirect('/public/users.php');
+exit;
