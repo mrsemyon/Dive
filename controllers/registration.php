a58c9296a48b3989afd3e54c71cd81b7d17fc6e5
mrSemyon <sky_net@mail.ru>
2022-06-09 05:08:15 +0500

    Added controller for registration

---
+++ controllers/registration.php
@@ -0,0 +1,20 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$pdo = createPDO();
+
+if (!empty(getUserByEmail($pdo, $_POST['email']))) {
+    setFlashMessage('danger', 'This email address is already taken by another user.');
+    redirect('/public/registration.php');
+    exit;
+}
+
+addUser($pdo, $_POST);
+
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['role'] = $_POST['role'];
+
+setFlashMessage('success', 'Registration successfull.');
+redirect("/public/users.php");
+exit;
