9fac7a1461d716e6b3b204a9d034bef0b1220b74
mrSemyon <syoma4k@gmail.com>
2022-06-16 22:53:31 +0500

    Added DB components to core

--- src/core.php
+++ src/core.php
@@ -5,3 +5,5 @@ session_start();
 require $_SERVER['DOCUMENT_ROOT'] . '/src/functions.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/src/classes.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/src/config.php';
+
+$db = new QueryBuilder(DBConnector::make($config['db']));
